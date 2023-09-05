7c102e61a066550a9cd9f08b3742be59826fde9d
Abdul Nasir <abdul.nasir55531@gmail.com>
2023-09-05 22:10:40 +0500

    creating a school management system in laravel

---
+++ resources/views/backend/user/edit_user.blade.php
@@ -0,0 +1,81 @@
+@extends('admin.admin_master')
+
+@section('admin')
+    <div class="content-wrapper">
+        <div class="container-full">
+            <!-- Main content -->
+            <section class="content">
+
+                <!-- Basic Forms -->
+                <div class="box">
+                    <div class="box-header with-border">
+                        <h4 class="box-title">Edit User</h4>
+                    </div>
+                    <!-- /.box-header -->
+                    <div class="box-body">
+                        <div class="row">
+                            <div class="col">
+                                <form method="POST" action="{{ route('user.update', $editData->id) }}">
+                                    @csrf
+                                    <div class="row">
+                                        <div class="col-12">
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <h5>User Role <span class="text-danger">*</span></h5>
+                                                        <div class="controls">
+                                                            <select name="usertype" id="select" required
+                                                                class="form-control">
+                                                                <option value="" disabled>Select User Role
+                                                                </option>
+                                                                <option {{ ($editData->usertype == 'Admin' ? 'selected' : '') }} value="Admin">Admin</option>
+                                                                <option {{ ($editData->usertype == 'User' ? 'selected' : '') }} value="User">User</option>
+                                                            </select>
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <h5>User Name <span class="text-danger">*</span></h5>
+                                                        <div class="controls">
+                                                            <input type="text" name="name" value="{{ $editData->name }}" class="form-control">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="row">
+                                                <div class="col-md-6">
+                                                    <div class="form-group">
+                                                        <h5>Email <span class="text-danger">*</span></h5>
+                                                        <div class="controls">
+                                                            <input type="email" name="email" value="{{ $editData->email }}" class="form-control">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+
+                                            <div class="form-group">
+                                                <div class="text-xs-right">
+                                                    <input type="submit" value="Save Changes" class="btn btn-rounded btn-success">
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                </form>
+
+                            </div>
+                            <!-- /.col -->
+                        </div>
+                        <!-- /.row -->
+                    </div>
+                    <!-- /.box-body -->
+                </div>
+                <!-- /.box -->
+
+            </section>
+            <!-- /.content -->
+            <!-- /.content -->
+
+        </div>
+    </div>
+@endsection
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\Backend\UserController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('auth.login');
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('admin.index');
+    })->name('dashboard');
+});
+
+Route::get('/admin/logout', [AdminController::class, 'Logout'])->name('admin.logout');
+
+//user management all routes
+Route::prefix('users')->group(function () {
+    Route::get('/view', [UserController::class, 'UserView'])->name('user.view');
+    Route::get('/add', [UserController::class, 'UserAdd'])->name('user.add');
+    Route::post('/store', [UserController::class, 'UserStore'])->name('user.store');
+    Route::get('/edit/{id}', [UserController::class, 'UserEdit'])->name('user.edit');
+    Route::post('/update/{id}', [UserController::class, 'UserUpdate'])->name('user.update');
+    Route::get('/delete/{id}', [UserController::class, 'UserDelete'])->name('user.delete');
+});
